2d724c264e00f7906ead335fdd7041a73fc769da
Alexey <suxovarov.alexey@gmail.com>
2023-11-02 11:46:05 +0300

    Add test_create_company

--- companies/tests.py
+++ companies/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+import pytest
+from djmoney.money import Money
 
-# Create your tests here.
+from .models import Company
+
+
+@pytest.mark.django_db
+def test_create_company():
+    company = Company.objects.create(
+        name='Alfa',
+        country='Russia',
+        industry='Finance',
+        share_capital=Money(1000000, 'USD')
+    )
+
+    assert company.name == 'Alfa'
