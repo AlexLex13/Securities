0dc92e764a91adcc90852cb7697e83963ec3e67d
Alexey <suxovarov.alexey@gmail.com>
2023-11-02 16:06:15 +0300

    Add redis-cache settings

--- securitieswebsite/settings.py
+++ securitieswebsite/settings.py
@@ -72,6 +72,16 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "KEY_PREFIX": "fin"
+    }
+}
+
+CACHE_TTL = 60 * 15
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
