9fedfbd944da450307090152273df6940ae581e0
Alexey <suxovarov.alexey@gmail.com>
2023-11-10 14:33:08 +0300

    Change task structure

--- userpreferences/tasks.py
+++ userpreferences/tasks.py
@@ -5,41 +5,42 @@ import dramatiq
 from django.template.loader import render_to_string
 from weasyprint import HTML
 
+BONDS_COLUMNS = [
+    'name', 'maturity_years', 'profitability', 'coupon_yield',
+    'coupon_yield_last', 'rating', 'volume', 'coupon_value',
+    'coupon_payments_frequency', 'accumulated_income', 'duration',
+    'price', 'next_coupon_date', 'issue_date', 'maturity_date',
+    'offer_date', 'company'
+]
+
+SHARES_COLUMNS = [
+    'name', 'ticker', 'last_price', 'price_change', 'volume',
+    'last_transaction_time', 'weekly_price_change', 'monthly_price_change',
+    'annual_price_change', 'capitalization', 'volume_change', 'company'
+]
+
 
 @dramatiq.actor
 def create_csv(response, user_preference):
     writer = csv.writer(response)
 
-    writer.writerow(['name', 'maturity_years', 'profitability', 'coupon_yield',
-                     'coupon_yield_last', 'rating', 'volume', 'coupon_value',
-                     'coupon_payments_frequency', 'accumulated_income', 'duration',
-                     'price', 'next_coupon_date', 'issue_date', 'maturity_date',
-                     'offer_date', 'company'])
+    writer.writerow(BONDS_COLUMNS)
 
     bonds = user_preference.bonds.all()
 
     writer.writerow(['Bonds'])
     for bond in bonds:
-        writer.writerow([bond.name, bond.maturity_years, bond.profitability, bond.coupon_yield,
-                         bond.coupon_yield_last, bond.rating, bond.volume, bond.coupon_value,
-                         bond.coupon_payments_frequency, bond.accumulated_income, bond.duration,
-                         bond.price, bond.next_coupon_date, bond.issue_date,
-                         bond.maturity_date, bond.offer_date, bond.company.name])
+        writer.writerow(bond.get_fields())
 
     writer.writerow([])
 
-    writer.writerow(['name', 'ticker', 'last_price', 'price_change', 'volume',
-                     'last_transaction_time', 'weekly_price_change', 'monthly_price_change',
-                     'annual_price_change', 'capitalization', 'volume_change', 'company'])
+    writer.writerow(SHARES_COLUMNS)
 
     shares = user_preference.shares.all()
 
     writer.writerow(['Shares'])
     for share in shares:
-        writer.writerow([share.name, share.ticker, share.last_price, share.price_change,
-                         share.volume, share.last_transaction_time, share.weekly_price_change,
-                         share.monthly_price_change, share.annual_price_change,
-                         share.capitalization, share.volume_change, share.company.name])
+        writer.writerow(share.get_fields())
 
 
 @dramatiq.actor
