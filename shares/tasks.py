0062f2f81cb8a0d6258009ad41f879b608e906f3
Alexey <suxovarov.alexey@gmail.com>
2023-11-19 16:49:20 +0300

    Add fetch shares tasks

---
+++ shares/tasks.py
@@ -0,0 +1,40 @@
+import datetime
+
+import dramatiq
+import requests
+from bs4 import BeautifulSoup
+
+
+@dramatiq.actor
+def fetch_shares():
+    url = 'https://smart-lab.ru/q/shares/'
+    headers = {'Content-Type': 'text/html'}
+    response = requests.get(url, headers=headers)
+
+    shares = []
+
+    bs = BeautifulSoup(response.text, 'lxml')
+
+    for i in (*list(range(1, 17)), *list(range(18, 42))):
+        share_fields = []
+        for j in (0, 1, 2, *list(range(6, 12)), 13, 15, 16):
+            share_fields.append(bs.find('tbody').find_all('tr')[i].find_all('td')[j].text.strip())
+        shares.append(share_fields)
+
+    return shares
+
+
+@dramatiq.actor
+def processing_shares(fields):
+    fields_list = [field.strip("[]'%").replace(' ', '') for field in fields.split(", ")[1:]]
+    for i, field in enumerate(fields_list):
+        print(i, fields_list[i])
+        if field == '-' or field == '':
+            fields_list[i] = None
+        elif i in (2, 3, 4, 6, 7, 8, 9, 10, 11):
+            fields_list[i] = float(field)
+        elif i == 5:
+            time_attr = [int(i) for i in field.split(":")]
+            fields_list[i] = datetime.time(*time_attr)
+
+    return fields_list
