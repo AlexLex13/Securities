a5a9ce93ef7d1984f4ad274c36c25ef16b67e7d4
Alexey <suxovarov.alexey@gmail.com>
2023-11-21 10:41:56 +0300

    Delete main page

--- securitieswebsite/urls.py
+++ securitieswebsite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='bonds')),
     path('bonds/', include('bonds.urls')),
     path('authentication/', include('authentication.urls')),
     path('preferences/', include('userpreferences.urls')),
