8412faddaf5245b591159a5f2df007cd36334eda
Alexey <suxovarov.alexey@gmail.com>
2023-11-21 22:52:09 +0300

    Add create_df

--- userpreferences/utils.py
+++ userpreferences/utils.py
@@ -1,3 +1,28 @@
+import pandas as pd
+
+from bonds.models import Bond
+from shares.models import Share
+
+
+def create_df(preference):
+    bonds = preference.bonds.all().select_related('company')
+    shares = preference.shares.all().select_related('company')
+
+    bonds_table = {}
+    for i, key in enumerate(Bond.FIELDS):
+        bonds_table[key] = [bond.get_fields()[i] for bond in bonds]
+
+    bonds_df = pd.DataFrame(bonds_table)
+
+    shares_table = {}
+    for i, key in enumerate(Share.FIELDS):
+        shares_table[key] = [share.get_fields()[i] for share in shares]
+
+    shares_df = pd.DataFrame(shares_table)
+
+    return bonds_df, shares_df
+
+
 def parse_bonds_df(df):
     bonds = []
     for lst in df.values:
